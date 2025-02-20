6a612206696c6f7492eabda76c3b379bcb1321af
aevam shingala <aevamshingala97@gmail.com>
2025-02-20 22:13:05 +0530

    bigchange

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,2 @@
 import Message from "./messege.js";
 export default Message;
-// export { default as Message } from "./messege.jsx";
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import Message from "./messege.jsx";
-
-export default Message;
-// export { default as Message } from "./messege.jsx";
